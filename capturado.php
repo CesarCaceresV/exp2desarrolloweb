58596b6fd515d60ed60ba63e92a3d3c6fdcbc9cb
CesarCaceresV <cesar.caceres@alumnos.uv.cl>
2017-09-26 09:56:50 -0500

    Add files via upload
    primer intento, me tuve que ir a FIa

---
+++ capturado.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+<form action="capturado.php" method="post">
+Nombre: <input type="text" name="name"><br>
+Apellido: <input type="text" name="last"><br>
+Fecha de Nacimiento: <input type="text" name="birthdate"><br>
+E-mail: <input type="text" name="email"><br>
+Direccion web personal: <input type="url" name="myurl">
+<input type="submit">
+</form>
+
+</body>
+</html>
